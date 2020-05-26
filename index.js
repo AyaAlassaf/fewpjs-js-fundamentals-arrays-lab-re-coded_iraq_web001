acf41c58671fdd435077ffc556143b26a4369ce5
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-26 17:55:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Write your solution here!
 const append=["Milo", "Otis", "Garfield"];
+append.push('Odie');
+const prepend=["Milo", "Otis", "Garfield"];
+
+const removelast=["Milo", "Otis", "Garfield"];
+const removeFirst=["Milo", "Otis", "Garfield"];
