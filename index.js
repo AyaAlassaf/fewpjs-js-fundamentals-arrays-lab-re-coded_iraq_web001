4a9f03a7faa195f670d254103fd1310255d07e58
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-26 17:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const append=["Milo", "Otis", "Garfield"];
 append.push('Odie');
 const prepend=["Milo", "Otis", "Garfield"];
-
-const removelast=["Milo", "Otis", "Garfield"];
+prepend.unshift('Odie');
+const removeLast=["Milo", "Otis", "Garfield"];
+removeLast.pop('Odie');
 const removeFirst=["Milo", "Otis", "Garfield"];
+removeFirst.shift('Odie');
